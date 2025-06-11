66f4ce001dc8d17b7dd641a870b5d73b3d0cc996
imujinu <wlsdnrhdwkd@gmail.com>
2025-06-11 10:29:01 +0900

    test

--- src/C01Basic/C04IFStatement.java
+++ src/C01Basic/C04IFStatement.java
@@ -1,4 +1,15 @@
 package C01Basic;
 
+import java.util.Scanner;
+
 public class C04IFStatement {
+    public static void main(String[] args) {
+        int answer =1234;
+        Scanner sc = new Scanner(System.in);
+        if(answer == sc.nextInt()) System.out.println("문이 열렸습니다.");
+        else System.out.println("비밀번호가 틀렸습니다.");
+
+        if
+
+    }
 }
